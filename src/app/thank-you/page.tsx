a4d63cdaa3743602d92c6a10d7c4c8282d0611a2
TOPOLSKY11 <topolsky.business@gmail.com>
2024-04-25 09:51:13 +0200

    Fitka-thank-You page
    Marek-Thank you page-dorobit este spacing

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,108 @@
+export default function ThankYo() {
+
+    return (
+        
+        <main className="h-screen relative">
+        <div className="absolute w-full top-0 left-0 ">
+
+        <svg className="w-full" height="783" viewBox="0 0 1280 783" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_f_579_605)">
+        <ellipse cx="659.5" cy="-428.5" rx="638.5" ry="611.5" fill="url(#paint0_linear_579_605)"/>
+        </g>
+        <defs>
+        <filter id="filter0_f_579_605" x="-579" y="-1640" width="2477" height="2423" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="300" result="effect1_foregroundBlur_579_605"/>
+        </filter>
+        <linearGradient id="paint0_linear_579_605" x1="420" y1="-344.5" x2="1003.01" y2="-350.925" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FE9800"/>
+        <stop offset="0.0001" stop-color="#F6970A"/>
+        <stop offset="0.625" stop-color="#8F5FCD"/>
+        <stop offset="1" stop-color="#E916FB"/>
+        </linearGradient>
+        </defs>
+        </svg>
+
+
+        </div>
+
+
+        <div className="absolute w-full flex justify-center mt-44 z-10">
+        <svg width="890" height="446" viewBox="0 0 890 446" fill="" xmlns="http://www.w3.org/2000/svg">
+        <path d="M889.925 445.074C889.925 386.645 878.417 328.789 856.057 274.808C833.698 220.827 800.925 171.779 759.61 130.464C718.294 89.1489 669.246 56.376 615.265 34.0164C561.284 11.6568 503.428 0.148435 445 0.148438C386.571 0.14844 328.715 11.6568 274.734 34.0164C220.753 56.376 171.705 89.149 130.39 130.464C89.0747 171.779 56.3017 220.827 33.9421 274.808C11.5825 328.789 0.0742136 386.646 0.0742188 445.074H8.97272C8.97271 387.814 20.2509 331.115 42.1633 278.214C64.0757 225.312 96.1932 177.245 136.682 136.756C177.171 96.2675 225.238 64.1499 278.139 42.2375C331.041 20.3251 387.74 9.04694 445 9.04694C502.26 9.04693 558.959 20.3251 611.86 42.2375C664.761 64.1499 712.828 96.2674 753.317 136.756C793.806 177.245 825.924 225.312 847.836 278.214C869.749 331.115 881.027 387.814 881.027 445.074H889.925Z" fill="url(#paint0_linear_579_607)"/>
+        <defs>
+        <linearGradient id="paint0_linear_579_607" x1="445" y1="0.148437" x2="442.194" y2="566.889" gradientUnits="userSpaceOnUse">
+        <stop offset="0.677886" stop-color="white" stop-opacity="0.15"/>
+        <stop offset="1" stop-color="white" stop-opacity="0"/>
+        </linearGradient>
+        </defs>
+        </svg>
+
+        </div>
+
+        <div className="absolute w-full flex mt-32 ml-56  ">
+        <svg width="138" height="148" viewBox="0 0 148 148" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M132.025 37.7214L113.164 18.8607C111.081 16.7774 107.703 16.7774 105.62 18.8607C103.537 20.944 103.536 24.3217 105.62 26.405L124.481 45.2657C126.564 47.349 129.942 47.349 132.025 45.2657C134.108 43.1824 134.108 39.8047 132.025 37.7214Z" fill="white" fill-opacity="0.8" stroke="white" stroke-opacity="0.8" stroke-width="5.33462"/>
+        <path d="M45.2661 124.481L26.4053 105.62C24.322 103.537 20.9443 103.537 18.861 105.62C16.7777 107.704 16.7777 111.081 18.861 113.165L37.7218 132.025C39.805 134.109 43.1827 134.109 45.266 132.025C47.3493 129.942 47.3494 126.564 45.2661 124.481Z" fill="white" fill-opacity="0.8" stroke="white" stroke-opacity="0.8" stroke-width="5.33462"/>
+        <path d="M71.6711 120.708L26.4053 75.4427C24.322 73.3594 20.9443 73.3594 18.861 75.4427C16.7777 77.526 16.7777 80.9037 18.861 82.987L64.1268 128.253C66.2101 130.336 69.5878 130.336 71.6711 128.253C73.7544 126.169 73.7544 122.792 71.6711 120.708Z" fill="white" fill-opacity="0.8" stroke="white" stroke-opacity="0.8" stroke-width="5.33462"/>
+        <path d="M82.9874 56.5822L56.5823 82.9873C54.499 85.0706 54.499 88.4483 56.5823 90.5316C58.6656 92.6149 62.0433 92.6149 64.1266 90.5316L90.5317 64.1265C92.615 62.0432 92.615 58.6655 90.5317 56.5822C88.4484 54.4989 85.0707 54.4989 82.9874 56.5822Z" fill="white" fill-opacity="0.8" stroke="white" stroke-opacity="0.8" stroke-width="5.33462"/>
+        <path d="M128.253 64.1264L82.9873 18.8607C80.904 16.7774 77.5263 16.7774 75.443 18.8607C73.3597 20.944 73.3597 24.3217 75.443 26.405L120.709 71.6707C122.792 73.754 126.17 73.754 128.253 71.6707C130.336 69.5874 130.336 66.2097 128.253 64.1264Z" fill="white" fill-opacity="0.8" stroke="white" stroke-opacity="0.8" stroke-width="5.33462"/>
+        </svg>
+
+        </div>
+
+        <div className="absolute w-full flex mt-72 ml-56">
+        <svg width="164" height="190" viewBox="0 0 164 190" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M57.9647 13.6679L29.3591 30.1834C26.1994 32.0076 25.1168 36.0479 26.941 39.2076C28.7653 42.3673 32.8056 43.4499 35.9653 41.6256L64.5709 25.1101C67.7306 23.2859 68.8132 19.2456 66.989 16.0859C65.1647 12.9262 61.1244 11.8436 57.9647 13.6679Z" fill="white" fill-opacity="0.6" stroke="white" stroke-opacity="0.6" stroke-width="6.60619"/>
+        <path d="M133.936 145.253L105.331 161.768C102.171 163.593 101.088 167.633 102.913 170.793C104.737 173.952 108.777 175.035 111.937 173.211L140.543 156.695C143.702 154.871 144.785 150.831 142.961 147.671C141.136 144.511 137.096 143.429 133.936 145.253Z" fill="white" fill-opacity="0.6" stroke="white" stroke-opacity="0.6" stroke-width="6.60619"/>
+        <path d="M137.887 112.459L69.2336 152.096C66.0739 153.921 64.9913 157.961 66.8156 161.121C68.6398 164.28 72.6801 165.363 75.8398 163.539L144.493 123.902C147.653 122.077 148.736 118.037 146.911 114.877C145.087 111.718 141.047 110.635 137.887 112.459Z" fill="white" fill-opacity="0.6" stroke="white" stroke-opacity="0.6" stroke-width="6.60619"/>
+        <path d="M64.8083 78.3694L87.9299 118.417C89.7542 121.577 93.7945 122.66 96.9542 120.835C100.114 119.011 101.196 114.971 99.3722 111.811L76.2505 71.7632C74.4263 68.6035 70.386 67.5209 67.2263 69.3452C64.0666 71.1694 62.984 75.2097 64.8083 78.3694Z" fill="white" fill-opacity="0.6" stroke="white" stroke-opacity="0.6" stroke-width="6.60619"/>
+        <path d="M88.3408 26.6419L19.6872 66.2791C16.5275 68.1033 15.4449 72.1436 17.2692 75.3033C19.0934 78.463 23.1337 79.5456 26.2934 77.7213L94.947 38.0842C98.1067 36.2599 99.1892 32.2196 97.365 29.0599C95.5407 25.9002 91.5005 24.8176 88.3408 26.6419Z" fill="white" fill-opacity="0.6" stroke="white" stroke-opacity="0.6" stroke-width="6.60619"/>
+        </svg>
+
+        </div>
+
+        <div className="absolute w-full left-[1080px] top-[260px]">
+        <svg width="132" height="132" viewBox="0 0 132 132" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M107.842 90.1577C94.5005 113.267 64.9513 121.184 41.8424 107.842C18.7335 94.5004 10.8158 64.9512 24.1578 41.8423C37.4997 18.7334 67.0489 10.8158 90.1578 24.1577C113.267 37.4996 121.184 67.0488 107.842 90.1577ZM32.3931 46.597C21.6771 65.1576 28.0364 88.891 46.5971 99.607C65.1577 110.323 88.8911 103.964 99.6071 85.403C110.323 66.8424 103.964 43.109 85.4031 32.393C66.8425 21.677 43.1091 28.0363 32.3931 46.597Z" fill="white" fill-opacity="0.42"/>
+        <path d="M82.4268 37.5469L65.9995 65.9997L72.1892 89.0997" stroke="white" stroke-opacity="0.42" stroke-width="8" stroke-linecap="round"/>
+        </svg>
+
+        </div>
+        
+        <div className="absolute bg-white py-16 px-12 mt-56 ml-96  items-center rounded-lg ">
+
+                
+                <p className="font-extrabold mt-2 text-5xl">
+                        Ďakujeme! 
+                    </p>  
+
+                    <p className="font-medium mt-12 text-1xl">
+                    Tvoja permanentka skončí: 
+                    </p>
+
+                    <p className="font-bold text-1xl ">
+                        20.3.2024
+                    </p>
+
+                    <p className="mt-4">
+                    Na tvoj mail (jozko@gmail.com) sme poslali QR kód ktorým sa jednoducho overíš vo fitku.
+                    </p>
+        
+
+
+        </div>
+        
+
+         
+         
+
+        
+        
+        
+
+            
+      </main>
+    );
+  }
